277df115ec7c8687dac534c4e22cc624fa31c652
Victor <yungaeronaut@gmail.com>
2025-01-02 22:48:15 +0100

    CLI calculator completed

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-console.log("I am writing typescript");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var firstStr = (0, readline_sync_1.question)("Enter first number: ");
+    var operator = (0, readline_sync_1.question)("Enter operator: ");
+    var secondStr = (0, readline_sync_1.question)("Enter the second string: ");
+    var validInput = isNumber(firstStr) && isOp(operator) && isNumber(secondStr);
+    if (validInput) {
+        var firstNum = parseInt(firstStr);
+        var secondNum = parseInt(secondStr);
+        var result = calculate(firstNum, operator, secondNum);
+        console.log(result);
+    }
+    else {
+        console.log("\ninvalid input\n");
+        main();
+    }
+    // const firstNum = isNumber(firstStr);
+    // console.log(firstNum + " its a num");
+    // const opera = isOp(operator);
+    // console.log(opera + " its operator");
+    // console.log(firstStr, operator, secondStr);
+}
+function calculate(firstNum, operator, secondNum) {
+    switch (operator) {
+        case "+":
+            return firstNum + secondNum;
+        case "-":
+            return firstNum - secondNum;
+        case "*":
+            return firstNum * secondNum;
+        case "/":
+            return firstNum / secondNum;
+    }
+}
+function isOp(operator) {
+    switch (operator) {
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+            return true;
+        default:
+            return false;
+    }
+}
+function isNumber(str) {
+    var maybeNum = parseInt(str);
+    var isNum = !isNaN(maybeNum);
+    return isNum;
+}
+main();
--- index.ts
+++ index.ts
@@ -1,1 +1,70 @@
-console.log("I am writing typescript");
+import { question } from 'readline-sync';
+
+type Operator = "+" | "-" | "*" | "/";
+
+function main(): void
+{
+    const firstStr: string = question("Enter first number: ");
+    const operator: string = question("Enter operator: ");
+    const secondStr: string = question("Enter the second string: ")
+
+    const validInput: boolean = isNumber(firstStr) && isOp(operator) && isNumber(secondStr);
+
+    if (validInput) 
+    {
+        const firstNum: number = parseInt(firstStr);
+        const secondNum: number = parseInt(secondStr);
+
+        const result = calculate(firstNum, operator as Operator, secondNum);
+        console.log(result);
+    }
+    else
+    {
+        console.log("\ninvalid input\n");
+        main();
+    }
+    
+    // const firstNum = isNumber(firstStr);
+    // console.log(firstNum + " its a num");
+
+    // const opera = isOp(operator);
+    // console.log(opera + " its operator");
+
+
+    // console.log(firstStr, operator, secondStr);
+}
+
+function calculate(firstNum: number, operator: Operator, secondNum: number)
+{
+    switch (operator) {
+        case "+":
+            return firstNum + secondNum;
+        case "-":
+            return firstNum - secondNum;
+        case "*":
+            return firstNum * secondNum;
+        case "/":
+            return firstNum / secondNum;
+
+    }
+}
+
+function isOp(operator: string): boolean
+{
+    switch(operator){
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+            return true;
+        default:
+            return false;
+    }
+}
+
+function isNumber(str: string): boolean {
+    const maybeNum = parseInt(str);
+    const isNum: boolean = !isNaN(maybeNum);
+    return isNum;
+}
+main();
